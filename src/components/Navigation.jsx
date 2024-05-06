5989628b9eec3b6a5cd47f1e2828d2d0e6f7e714
DenysVM <muratov.d@gmail.com>
2024-05-06 12:08:48 +0200

    1.9.1 Navigation&NavLinks refactoring for desktop

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,8 @@ import {
   useColorMode,
   Stack,
   useDisclosure,
-  IconButton
+  IconButton,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useTranslation } from 'react-i18next';
@@ -16,7 +17,7 @@ import UserSection from './UserSection';
 import ProductCart from './Product/ProductCart'; 
 import Logo from './common/Logo';
 import NavLinks from './common/NavLinks';
-import BurgerMenu from './common/BurgerMenu'; // Импорт нового компонента BurgerMenu
+import BurgerMenu from './common/BurgerMenu';
 
 const Navigation = () => {
   const { isOpen, onToggle, onClose } = useDisclosure();
@@ -24,10 +25,16 @@ const Navigation = () => {
   useOutsideClick(menuRef, () => isOpen && onToggle());
   const { colorMode, toggleColorMode } = useColorMode();
   const { t } = useTranslation(); 
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
     <Box ref={menuRef} px={4} bg={colorMode === 'light' ? 'gray.100' : 'gray.700'} color={colorMode === 'light' ? 'gray.600' : 'white'}>
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      {!isMobile && (
+          <Flex flexShrink={0}>
+            <NavLinks onNavigate={onClose} />
+          </Flex>
+        )}
         <BurgerMenu isOpen={isOpen} toggleMenu={onToggle} label={t('openMenu')} />
         <Logo />
         <Flex alignItems={'center'}>
@@ -35,16 +42,16 @@ const Navigation = () => {
           <LanguageSelector />
           <ProductCart />
           <UserSection />
-        </Flex>
+          </Flex>
       </Flex>
 
       {isOpen && (
-        <Box pb={4} display={{ base: 'block', md: 'none' }}>
+        <Box pb={4} display={{ baseм: 'block', md: 'none' }}>
           <Stack spacing={4} as="nav">
             <NavLinks onNavigate={onClose} />
           </Stack>
         </Box>
-      )}
+              )}
     </Box>
   );
 };
--- src/components/common/NavLinks.jsx
+++ src/components/common/NavLinks.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 const NavLinks = ({ onNavigate }) => {
   const { t } = useTranslation();
-  
+
   return (
-    <>
+    <Stack
+      direction={{ base: 'column', md: 'row' }}
+      spacing={4}
+    >
       <Text as={RouterLink} to="/" onClick={onNavigate}>{t('home')}</Text>
       <Text as={RouterLink} to="/catalog" onClick={onNavigate}>{t('products')}</Text>
-    </>
+    </Stack>
   );
 };
 
