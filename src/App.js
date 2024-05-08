70f37e8856660f4c20f0984dc57ec335fb2c328a
DenysVM <muratov.d@gmail.com>
2024-05-08 10:47:01 +0200

    Routes fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 import { store } from './store/store';
 import Navigation from './components/Navigation';
 import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
+import AccountPage from './pages/AccountPage';
 import { CartProvider, CartPage } from './components/Cart';
 
 import './i18n/i18n';
@@ -20,6 +21,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/catalog" element={<CatalogPage />} />
+              <Route path="/account" element={<AccountPage />} />
               <Route path="/cart" element={<CartPage />} />
             </Routes>
           </Router>
